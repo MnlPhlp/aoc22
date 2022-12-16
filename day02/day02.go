2ddf41cc862adf2f8194d0745ec37ba99b1e54d4
MnlPhlp <online@philipp-manuel.de>
2022-12-16 17:28:40 +0100

    day02

---
+++ day02/day02.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	ROCK = iota
+	PAPER
+	SCISSORS
+)
+
+func getMove(move byte) byte {
+	if move < 'D' {
+		return move - 'A'
+	} else {
+		return move - 'X'
+	}
+}
+
+func calcScore(line string) int {
+	opponent := getMove(line[0])
+	self := getMove(line[2])
+	// score for pick
+	score := int(self + 1)
+	// score for draw
+	if self == opponent {
+		score += 3
+	}
+	// score for winning
+	if (self+2)%3 == opponent {
+		score += 6
+	}
+	return score
+}
+
+func decideMove(opponent, self byte) byte {
+	// start with draw move
+	move := opponent
+	switch self {
+	case 'X':
+		// pick losing move
+		move = (opponent + 2) % 3
+	case 'Z':
+		// pick winning move
+		move = (opponent + 1) % 3
+	}
+	return move
+}
+
+func calcScore2(line string) int {
+	opponent := getMove(line[0])
+	self := decideMove(opponent, line[2])
+	// score for pick
+	score := int(self + 1)
+	// score for draw
+	if self == opponent {
+		score += 3
+	}
+	// score for winning
+	if (self+2)%3 == opponent {
+		score += 6
+	}
+	return score
+}
+
+func main() {
+	input, _ := os.ReadFile("input.txt")
+	lines := strings.Split(string(input), "\n")
+	score := 0
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		score += calcScore(line)
+	}
+	fmt.Printf("Score 1: %v\n", score)
+
+	score = 0
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		score += calcScore2(line)
+	}
+	fmt.Printf("Score 2: %v\n", score)
+}
